b206824d30669836fe93aebaf9e30d906cb36507
Julian Rüth <julian.rueth@fsfe.org>
2024-07-11 13:50:06 +0300

    Start scheduler on random port

--- plafrim/spawn-scheduler.sh
+++ plafrim/spawn-scheduler.sh
@@ -6,5 +6,5 @@ conda activate flatsurvey
 
 SCHEDULER=/beegfs/jrueth/scheduler.$1.json
 
-dask-scheduler --scheduler-file=$SCHEDULER --port 38325
+dask-scheduler --scheduler-file=$SCHEDULER --port=`shuf -i 32768-60999 -n 1`
 
