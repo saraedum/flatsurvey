f09e10641cc4228467a7a52a96802c2da954e534
Julian Rüth <julian.rueth@fsfe.org>
2025-10-06 04:02:49 +0300

    Fix join invocation on cluster

--- plafrim/postprocess.sh
+++ plafrim/postprocess.sh
@@ -4,6 +4,7 @@ shopt -s globstar
 # Go to the flatsurvey root directory.
 cd "$(dirname "$0")"/..
 
+# Merge all orbit closure results into history/orbit-closure.json
 cp -R /beegfs/jrueth/flatsurvey/history/ /beegfs/jrueth/flatsurvey/backup
-salloc --ntasks=1 --time=72:00:00 srun --pty pixi run flatsurvey-maintenance join /beegfs/jrueth/flatsurvey/$1/**/*.json /beegfs/jrueth/flatsurvey/history/**/*.json --outdir /beegfs/jrueth/flatsurvey/history/$1
+salloc --ntasks=1 --time=72:00:00 srun --pty pixi run flatsurvey-maintenance join  --outdir /beegfs/jrueth/flatsurvey/history/$1 /beegfs/jrueth/flatsurvey/$1/**/*.json /beegfs/jrueth/flatsurvey/history/**/*.json
 rm /beegfs/jrueth/flatsurvey/$1/**/*.json
